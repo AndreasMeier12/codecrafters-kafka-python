3beff82243feeb3a3de44a44a8d91ec787375d99
Andreas Meier <meier89@gmx.ch>
2025-04-08 17:05:48 +0200

    Parse correlation ID

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import socket  # noqa: F401
 import struct
+from dataclasses import dataclass
 
 
 def create_message(id):
@@ -11,6 +12,22 @@ def handle_client(client: socket):
     client.recv(1024)
     client.sendall(create_message(7))
     client.close()
+
+@dataclass
+class KafkaHeader:
+    message_size: int
+    request_api_key: int
+    request_api_version: int
+    correlation_id: int
+
+    @staticmethod
+    def of(msg: bytes):
+        message_size = int.from_bytes(msg[0:4], "big")
+        request_api_key = int.from_bytes(msg[4:6], "big")
+        request_api_version = int.from_bytes(msg[6:8], "big")
+        correlation_id = int.from_bytes(msg[8:12], "big")
+        return KafkaHeader(message_size, request_api_key, request_api_version, correlation_id)
+
 
 
 def main():
@@ -26,8 +43,9 @@ def main():
     accepted_socket, _ = server.accept()  # wait for client
 
     while msg := accepted_socket.recv(1024):
+        header: KafkaHeader = KafkaHeader.of(msg)
 
-        accepted_socket.sendall(struct.pack(">II", 0, 7))
+        accepted_socket.sendall(struct.pack(">q",header.correlation_id))
 
 
 
