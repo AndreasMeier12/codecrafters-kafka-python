a7711c69d5b3ac63f98516d92aaaa599ecb9aa5c
Andreas Meier <meier89@gmx.ch>
2025-04-21 09:32:49 +0200

    Continue parsing metadata

--- app/metadata/metadata.py
+++ app/metadata/metadata.py
@@ -46,6 +46,10 @@ class ClusterMetaDataLog:
     max_timestamp: datetime.datetime
     producer_id: int
 
+@dataclass
+class RecordValue:
+
+
 
 
 
@@ -63,26 +67,42 @@ class ClusterMetaDataLog:
 
         base_offset = parser.read(8)
 
-        batch_length = int.from_bytes(in_file.read(4))
-        partition_leader_epoch = int.from_bytes(in_file.read(4))
-        magic_byte = int.from_bytes(in_file.read(1))
-        crc = int.from_bytes(in_file.read(4))
-        record_batch = int.from_bytes(in_file.read(2))
+        batch_length = parser.read(4)
+        partition_leader_epoch = parser.read(4)
+        magic_byte = parser.read(1)
+        crc = parser.read(4)
+        record_batch = parser.read(2)
         compression = Compression.of(record_batch & 0x0003)
         timestamp_type = record_batch & 0x0004
         is_transactional = (record_batch & 0x0008) > 0
         is_control_batch = (record_batch & 0x000f) > 0
         has_delete_horizon = (record_batch & 0x0010) > 0
-        last_offset_data = int.from_bytes(in_file.read(4))
+        last_offset_data = parser.read(4)
 
-        timestamp_raw = int.from_bytes(in_file.read(8))
+        timestamp_raw = parser.read(8)
         base_timestamp: datetime.datetime.fromtimestamp(timestamp_raw)
-        timestamp_raw = int.from_bytes(in_file.read(8))
+        timestamp_raw = parser.read(8)
         max_timestamp: datetime.datetime.fromtimestamp(timestamp_raw)
-        producer_id = int.from_bytes(in_file.read(8))
-        producer_epoch = int.from_bytes(in_file.read(4))
-        base_sequence = int.from_bytes(in_file.read(4))
-        records_length: int.from_bytes(in_file.read(4))
+        producer_id = parser.read(8)
+        producer_epoch = parser.read(4)
+        base_sequence = parser.read(4)
+        records_length = parser.read(4)
+        records: list[Record]
+        for i in range(records_length):
+            record_length = parser.read(1, signed=True)
+            attributes = parser.read(1)
+            timestamp_delta = parser.read(1, signed=True)
+            offset_delta = parser.read(1, signed=True)
+            key_length = parser.read(1, signed=True)
+            key = None
+            if key_length >= 0:
+                key = parser.read(key_length)
+            value_length = parser.read(1, signed=True)
+            value = None
+            if value_length > 0:
+                value = parser.read(value_length)
+
+
 
 class _Parser:
 
